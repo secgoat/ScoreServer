b5aa71b0bb185ff729ca0a8352431798ff618da2
sec_goat <joshua.tilson@yahoo.com>
2012-08-29 07:32:00 -0400

    Added a logoff controller, really simple considering. also started workign on a database pull to check username / password.

---
+++ ScoreServerMVC/Controllers/LogOffController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace ScoreServerMVC.Controllers
+{
+    public class LogOffController : Controller
+    {
+        //
+        // GET: /LogOff/
+
+        public ActionResult Index()
+        {
+            FormsAuthentication.SignOut();
+            return View();
+
+        }
+
+    }
+}
--- ScoreServerMVC/Controllers/LogonController.cs
+++ ScoreServerMVC/Controllers/LogonController.cs
@@ -4,11 +4,13 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Security;
+using ScoreServerMVC.Models;
 
 namespace ScoreServerMVC.Controllers
 {
     public class LogonController : Controller
     {
+        ScoreDbContext db = new ScoreDbContext();
         //
         // GET: /Logon/
 
@@ -22,11 +24,20 @@ namespace ScoreServerMVC.Controllers
         {
             if (ModelState.IsValid)
             {
-                if (model.Username == "bob" && model.Password == "bob") //simulate DB call where username and password valid
+                Users user = db.Users.Select() (model.Username); //TODO: need to figure out how to select a user row by ID based on username
+                if (user != null)
+                {
+                    if (user.ValidatePassword(model.Password))
+                    {
+                        FormsAuthentication.SetAuthCookie(model.Username, false); //set non persistant cookie,
+                        return RedirectToAction("Index", "Home"); //return to home page
+                    }
+                }
+               /* if (model.Username == "bob" && model.Password == "bob") //simulate DB call where username and password valid
                 {
                     FormsAuthentication.SetAuthCookie(model.Username, false); //set non persistant cookie,
                     return RedirectToAction("Index", "Home"); //return to home page
-                }
+                }*/
                 else
                 {
                     ModelState.AddModelError("", "Invalid Username or Password"); //if not valid or not authenticated return error
