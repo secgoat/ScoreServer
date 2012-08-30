ae92962d0b89305e2c1d28823f228de53b0941fa
unknown <joshua.tilson@yahoo.com>
2012-08-30 19:55:22 -0400

    just commit already

---
+++ ScoreServerMVC/RoleMemershipCheck.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ScoreServerMVC.Models;
+
+//TODO: check the cookie I think this one has the basics on this http://stackoverflow.com/questions/1149996/storing-more-information-using-formsauthentication-setauthcookie
+
+namespace ScoreServerMVC
+{
+    public static class RoleMemershipCheck
+    {
+        ScoreDbContext db = new ScoreDbContext();
+
+        public static bool IsAdmin(string userName)
+        {
+
+            return false; 
+        }
+
+    }
+}
