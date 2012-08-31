c79c3f7b481e92188b8580fbee800bff8a0ac0a5
sec_goat <joshua.tilson@yahoo.com>
2012-08-31 07:07:02 -0400

    added a couple of lines

--- ScoreServerMVC/RoleMemershipCheck.cs
+++ ScoreServerMVC/RoleMemershipCheck.cs
@@ -5,7 +5,7 @@ using System.Web;
 using ScoreServerMVC.Models;
 
 //TODO: check the cookie I think this one has the basics on this http://stackoverflow.com/questions/1149996/storing-more-information-using-formsauthentication-setauthcookie
-
+//http://stackoverflow.com/questions/7220184/read-form-authentication-cookie-from-asp-net-code-behind
 namespace ScoreServerMVC
 {
     public static class RoleMemershipCheck
